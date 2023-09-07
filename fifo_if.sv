790887b1547e16003ac1fd6da63d903a689244b2
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-07 11:47:49 +0530

    Update fifo_if.sv

--- fifo_if.sv
+++ fifo_if.sv
@@ -1,17 +1,19 @@
 `include "fifo_define.sv";
 
-interface fifo_if;
-    logic clk, rstn,i_wren,i_rden;
+interface fifo_if(input bit clk, rstn);
+    logic i_wren,i_rden;
     logic  [DATA_W - 1 : 0] i_wrdata;
     logic [DATA_W - 1 : 0] o_rddata;
     logic  o_alm_full , o_full,  o_alm_empty, o_empty;
 
     //clocking block for driver 
-//     clocking drv_cb@(posedge clk);
-//         default input #0 output #0;
-//    endclocking
+    clocking drv_cb@(posedge clk);
+        default input #0 output #0;
+        output i_wren, i_rden , i_wrdata;
+        input rstn ;
+   endclocking
 
-// modport DRV(clocking drv_cb);
+modport DRV(clocking drv_cb);
     
 endinterface : fifo_if
  
