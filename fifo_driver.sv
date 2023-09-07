74604933ace76b601f6c4065f63766419b6edebb
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-07 11:58:46 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,10 +1,11 @@
 `include "fifo_define.sv"
+`define DRV vif.DRV.drv_cb
 
 class fifo_driver extends uvm_driver#(fifo_transaction);
   
   `uvm_component_utils(fifo_driver)
   
-   virtual fifo_interface vif;
+   virtual fifo_if vif;
   
   function new(string name, uvm_component parent = null);
     super.new(name,parent);
@@ -12,27 +13,22 @@ class fifo_driver extends uvm_driver#(fifo_transaction);
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual fifo_interface)::get(this, "", "vif", vif)) 
+    if(!uvm_config_db#(virtual fifo_if)::get(this, "", "vif", vif)) 
       begin
         `uvm_fatal("NO_VIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
       end
   endfunction
   
-  //---------------------------------------
-  //Run Phase
-  //---------------------------------------
-  virtual task run_phase(uvm_phase phase);
+  task run_phase(uvm_phase phase);
     forever begin
       fifo_transaction trans;
       seq_item_port.get_next_item(trans);
       uvm_report_info("FIFO_DRIVER ", $psprintf("Got Transaction %s",trans.convert2string()));
       
-      @(`DRIV_IF);
-      //---------------------------------------
-      //Driver's writing logic
-      //---------------------------------------
+      @(`DRV);
+     
       if(trans.wr) begin
-        `DRIV_IF.wr<=trans.wr;
+        `DRV.wr<=trans.wr;
         `DRIV_IF.rd<=trans.rd;
         `DRIV_IF.data_in<=trans.data_in;
         @(`DRIV_IF);begin
