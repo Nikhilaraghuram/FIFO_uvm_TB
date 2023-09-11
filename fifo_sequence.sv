dbcb854442e91ffe1308900fd06bc999bde9ec56
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-11 14:59:18 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -12,7 +12,7 @@ class fifo_sequence extends uvm_sequence#(fifo_transaction);
    
    // Task Body. Behavior of the sequence. 
   task body();
-     repeat(10) 
+     repeat(DEPTH) 
      begin
        
        tx=fifo_transaction::type_id::create("tx");
@@ -26,3 +26,83 @@ class fifo_sequence extends uvm_sequence#(fifo_transaction);
    endtask:body 
    
  endclass:fifo_sequence 
+// writing testcase
+
+class write_sequence extends fifo_sequence #(fifo_transaction);
+   `uvm_object_utils(write_sequence)
+
+   fifo_transaction tx;
+  
+   function new(string name="write_sequence");
+     super.new(name);
+   endfunction:new
+   
+   // Task Body. Behavior of the sequence. 
+  task body();
+     repeat(DEPTH) 
+     begin
+       
+       tx=fifo_transaction::type_id::create("tx");
+       
+       start_item(tx);
+        i_wren = 1'b1;
+        i_rden = 1'b0;
+       finish_item(tx);
+      
+     end
+   endtask:body 
+   
+ endclass:write_sequence 
+//read sequence 
+class read_sequence extends fifo_sequence #(fifo_transaction);
+   `uvm_object_utils(read_sequence)
+
+   fifo_transaction tx;
+  
+   function new(string name="read_sequence");
+     super.new(name);
+   endfunction:new
+   
+   // Task Body. Behavior of the sequence. 
+  task body();
+     repeat(DEPTH) 
+     begin
+       
+       tx=fifo_transaction::type_id::create("tx");
+       
+       start_item(tx);
+        i_wren = 1'b0;
+        i_rden = 1'b1;
+       finish_item(tx);
+      
+     end
+   endtask:body 
+   
+ endclass:read_sequence 
+
+// parallel write read sequence 
+class write_read_sequence extends fifo_sequence #(fifo_transaction);
+   `uvm_object_utils(write_read_sequence)
+
+   fifo_transaction tx;
+  
+   function new(string name="write_read_sequence");
+     super.new(name);
+   endfunction:new
+   
+   // Task Body. Behavior of the sequence. 
+  task body();
+     repeat(DEPTH) 
+     begin
+       
+       tx=fifo_transaction::type_id::create("tx");
+       
+       start_item(tx);
+        i_wren = 1'b1;
+        i_rden = 1'b1;
+       finish_item(tx);
+      
+     end
+   endtask:body 
+   
+ endclass:write_read_sequence 
