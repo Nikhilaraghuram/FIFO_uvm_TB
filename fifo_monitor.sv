e451ed260781367e7f3349d1298de20ebea2b5e4
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-12 13:07:29 +0530

    Update fifo_monitor.sv

--- fifo_monitor.sv
+++ fifo_monitor.sv
@@ -1,11 +1,13 @@
 `include "fifo_define.sv"
-`define MON vif.MON.drv_cb
+`define MON vif.MON.mon_cb
 
 class fifo_monitor extends uvm_monitor;
   
   virtual fifo_interface vif;
- 
-  uvm_analysis_port#(fifo_seq_item)ap;
+
+  fifo_transaction trans;
+  
+  uvm_analysis_port#(fifo_transaction) ap;
   
   `uvm_component_utils(fifo_monitor)
   
@@ -21,3 +23,30 @@ class fifo_monitor extends uvm_monitor;
        `uvm_error("build_phase", "No virtual interface specified for this monitor instance")
        end
    endfunction
+
+   virtual task run_phase(uvm_phase phase);
+    forever begin
+      @(`MON)
+      if(`MON == 1)begin
+        $display("\n write enable  is high");
+        trans.i_wrdata = `MON.i_wrdata;
+        trans.i_wren = 'b1;
+        trans.i_rden = 'b0;
+        trans.o_alm_full = `MON.o_alm_full;
+        trans.o_full = `MON.o_full;
+       
+        ap.write(trans);
+      end
+      else if(`MON.i_rden == 1)begin
+        @(`MON)
+        $display("\nRean enable  is high");
+        trans.o_rddata = `MON.o_rddata;
+        trans.i_rden= 'b1;
+        trans.i_wren = 'b0;
+         trans.o_alm_empty = `MON.o_alm_empty;
+        trans.o_empty = `MON.o_empty;
+        ap.write(trans);
+      end
+    end
+  endtask
+endclass
