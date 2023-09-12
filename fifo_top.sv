33c23aa3acd723ad1c4d300efd2420ecce3995dc
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-12 16:02:38 +0530

    Update fifo_top.sv

--- fifo_top.sv
+++ fifo_top.sv
@@ -20,7 +20,16 @@ module fifo_top;
 
   uvm_config_db#(virtual fifo_if)::set(uvm_root::get(), "*", "vif", vif);
 
-  //dut instantiation 
+  fifo dut(.clk(vif.clk),
+           .rstn(vif.rstn),
+           .i_wrdata(vif.i_wrdata),
+           .i_wren(vif.i_wren),
+           .i_rden(vif.rden),
+           .o_full(vif.full),
+           .o_empty(vif.o_empty),
+           .o_rddata(vif.o_rddata)
+           .o_alm_full(vif.o_alm_full)
+           .o_alm_empty(vif.o_alm_empty));
 
    initial begin
     uvm_config_db#(virtual f_interface)::set(null, "", "vif", tif);
