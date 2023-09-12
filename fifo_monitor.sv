39eb28879e1786867f483810c3bc436b1c84550d
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-12 12:31:43 +0530

    Update fifo_monitor.sv

--- fifo_monitor.sv
+++ fifo_monitor.sv
@@ -1,20 +1,23 @@
 `include "fifo_define.sv"
 `define MON vif.MON.drv_cb
 
-class fifo_monitor extends uvm_driver#(fifo_transaction);
+class fifo_monitor extends uvm_monitor;
   
-  `uvm_component_utils(fifo_driver)
+  virtual fifo_interface vif;
+ 
+  uvm_analysis_port#(fifo_seq_item)ap;
   
-   virtual fifo_if vif;
+  `uvm_component_utils(fifo_monitor)
   
+
   function new(string name, uvm_component parent = null);
-    super.new(name,parent);
+    super.new(name, parent);
+    ap=new("ap", this);
   endfunction
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual fifo_if)::get(this, "", "vif", vif)) 
-      begin
-        `uvm_fatal("NO_VIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
-      end
-  endfunction
+    if (!uvm_config_db#(virtual fifo_interface)::get(this, "", "vif", vif)) begin
+       `uvm_error("build_phase", "No virtual interface specified for this monitor instance")
+       end
+   endfunction
