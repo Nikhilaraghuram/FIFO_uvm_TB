d26a0d6fdde836e66e1231892c7ca0ef4c528cf6
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-12 15:42:19 +0530

    Create fifo_pkg.sv

---
+++ fifo_pkg.sv
@@ -0,0 +1,11 @@
+package fifo_pkg;
+  `include "fifo_transaction.sv"
+  `include "fifo_sequence.sv"
+  `include "fifo_sequencer.sv"
+  `include "fifo_driver.sv"
+  `include "fifo_monitor.sv"
+   `include "fifo_agent.sv"
+  `include "fifo_scoreboard.sv"
+  `include "fifo_env.sv"
+  `include "fifo_test.sv"
+endpackage
